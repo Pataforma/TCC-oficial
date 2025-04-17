0969e0bd5273afe57826afa46db88ed22e123e24
Johan Santos <johanvictor17@gmail.com>
2025-04-17 17:21:38 -0300

    Mudanças no Carrossel

--- src/components/CarrouselSection.jsx
+++ src/components/CarrouselSection.jsx
@@ -4,97 +4,104 @@ import Carousel from "react-bootstrap/Carousel"; // Usando o carousel do Bootstr
 function CarrouselSection() {
   return (
     <section className="py-5">
-      <h3 className="text-center mb-4">Nossos Carrosséis</h3>
-      <Carousel>
-      <Carousel.Item>
-        <div className="d-flex justify-content-center gap-3">
-              <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\petadocao1.jpg"
-            alt="Anúncio de pet perdido 1"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\petadocao2.jpg"
-            alt="Anúncio de pet perdido 2"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\petadocao3.avif"
-            alt="Anúncio de pet perdido 3"
-          />
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <h3 className="m-0">Nossos Carrosséis</h3>
+          <button className="btn btn-paleta">Ver todos</button>
         </div>
-          <Carousel.Caption>
-            <section>
-            <h3 style={{ textShadow: "1px 1px 4px black", color: "var(--elements-color)"}}>Pets para Adoção</h3>
-            <p>Ofereça um lar para um amigo.</p>
-            <button className="btn btn-secondary">Ver todos</button>
-            </section>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <div className="d-flex justify-content-center gap-3">
+        <Carousel 
+          indicators={true}
+          controls={true}
+          className="custom-carousel"
+          interval={5000}
+        >
+          <Carousel.Item>
+            <div className="d-flex justify-content-center gap-3">
               <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\vetparceiro.jpg"
-            alt="Veterinário 1"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\vetparceiro2.jpg"
-            alt="Veterinário 2"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\vetparceiro3.png"
-            alt="Veterinário 3"
-          />
-        </div>
-          <Carousel.Caption>
-            <section>
-            <h3 style={{ textShadow: "1px 1px 4px black", color: "var(--elements-color)" }}>Veterinários Parceiros</h3>
-            <p>Veja os veterinários mais próximos de você!</p>
-            <button className="btn btn-secondary">Ver todos</button>
-            </section>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <div className="d-flex justify-content-center gap-3">
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\petadocao1.jpg"
+                alt="Anúncio de pet perdido 1"
+              />
               <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover"}}
-            src="src\assets\imgs\petperdido1.webp"
-            alt="Anúncio de pet perdido 1"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\petperdido2.webp"
-            alt="Anúncio de pet perdido 2"
-          />
-          <img
-            className="img-fluid"
-            style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
-            src="src\assets\imgs\petperdido3.png"
-            alt="Anúncio de pet perdido 3"
-          />
-        </div>
-          <Carousel.Caption>
-            <section>         
-            <h3 style={{ textShadow: "1px 1px 4px black", color: "var(--elements-color)"}}> Pets Perdidos</h3>
-            <p>Ajude a encontrar animais perdidos.</p>
-            <button className="btn btn-secondary">Ver todos</button>
-            </section>
-          </Carousel.Caption>
-        </Carousel.Item>
-        {/* Outros carrosséis */}
-      </Carousel>
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\petadocao2.jpg"
+                alt="Anúncio de pet perdido 2"
+              />
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\petadocao3.avif"
+                alt="Anúncio de pet perdido 3"
+              />
+            </div>
+            <div className="carousel-caption-container">
+              <div className="carousel-caption-custom">
+                <h3>Pets para Adoção</h3>
+                <p>Ofereça um lar para um amigo.</p>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="d-flex justify-content-center gap-3">
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\vetparceiro.jpg"
+                alt="Veterinário 1"
+              />
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\vetparceiro2.jpg"
+                alt="Veterinário 2"
+              />
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\vetparceiro3.png"
+                alt="Veterinário 3"
+              />
+            </div>
+            <div className="carousel-caption-container">
+              <div className="carousel-caption-custom">
+                <h3>Veterinários Parceiros</h3>
+                <p>Veja os veterinários mais próximos de você!</p>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="d-flex justify-content-center gap-3">
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover"}}
+                src="src\assets\imgs\petperdido1.webp"
+                alt="Anúncio de pet perdido 1"
+              />
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\petperdido2.webp"
+                alt="Anúncio de pet perdido 2"
+              />
+              <img
+                className="img-fluid"
+                style={{ width: "300px", height: "300px", borderRadius: "5px", objectFit: "cover" }}
+                src="src\assets\imgs\petperdido3.png"
+                alt="Anúncio de pet perdido 3"
+              />
+            </div>
+            <div className="carousel-caption-container">
+              <div className="carousel-caption-custom">
+                <h3>Pets Perdidos</h3>
+                <p>Ajude a encontrar animais perdidos.</p>
+              </div>
+            </div>
+          </Carousel.Item>
+          {/* Outros carrosséis */}
+        </Carousel>
+      </div>
     </section>
   );
 }
